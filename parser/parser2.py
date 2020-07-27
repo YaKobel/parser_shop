edcc81893f2bfecb368a736d7a526bf76a839a54
Yaroslav Kobel <yarokobel@gmail.com>
2020-07-27 08:17:35 +0300

    add page pagination

--- parser/parser2.py
+++ parser/parser2.py
@@ -31,8 +31,15 @@ def get_content(html):
     return cards
 
 def parser():
+    PAGENATION = input('Укажите количество страниц для парсинга: ')
+    PAGENATION = int(PAGENATION.strip())
     html = get_html(URL)
     if html.status_code == 200:
+        cards = []
+        for page in range(1, PAGENATION):
+            print(f'Парсим страницу: {page}')
+            html = get_html(URL, params={'page': page})
+            cards.extend(get_content(html.text))
         pass
     else:
         print('Error')
